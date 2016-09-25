974b15fdbf5b3251965f97147f21b6488bf6421a
Vatsal-Bajpai <vatsal.bajpai05@gmail.com>
2016-09-25 23:23:03 +0530

    feat: Replaced listView with recyclerView chore: added recyclerView dependency

---
+++ app/src/main/java/org/adapters/viewHolders/ChatViewHolder.java
@@ -0,0 +1,41 @@
+package org.adapters.viewHolders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import org.fossasia.susi.ai.R;
+
+import static org.adapters.recyclerAdapters.ChatFeedRecyclerAdapter.MY_IMAGE;
+import static org.adapters.recyclerAdapters.ChatFeedRecyclerAdapter.MY_MESSAGE;
+import static org.adapters.recyclerAdapters.ChatFeedRecyclerAdapter.OTHER_IMAGE;
+import static org.adapters.recyclerAdapters.ChatFeedRecyclerAdapter.OTHER_MESSAGE;
+
+/**
+ * Created by
+ * --Vatsal Bajpai on
+ * --25/09/16 at
+ * --9:51 PM
+ */
+
+public class ChatViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView chatTextView;
+
+    public ChatViewHolder(View view, int myMessage) {
+        super(view);
+
+        switch (myMessage) {
+            case MY_MESSAGE:
+                chatTextView = (TextView) view.findViewById(R.id.text);
+                break;
+            case OTHER_MESSAGE:
+                chatTextView = (TextView) view.findViewById(R.id.text);
+                break;
+            case MY_IMAGE:
+            case OTHER_IMAGE:
+            default:
+        }
+
+    }
+}
