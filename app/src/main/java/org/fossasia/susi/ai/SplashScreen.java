8a2d10be7825147d0728a95bc4ca56becbb11210
Saurabh Singhal <singhalsaurabh95@gmail.com>
2016-09-27 23:48:34 +0530

    fixes multiple performance issues with splash screen.
    * Activity now extends AppcompatActivity
    * Image is now loaded dynamically.
    * Improved layout by decreasing one level.

--- app/src/main/java/org/fossasia/susi/ai/SplashScreen.java
+++ app/src/main/java/org/fossasia/susi/ai/SplashScreen.java
@@ -1,19 +1,30 @@
 package org.fossasia.susi.ai;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by jigyasa on 21/08/16.
  */
 
-public class SplashScreen extends Activity {
+public class SplashScreen extends AppCompatActivity {
+    @BindView(R.id.splash_image)
+    ImageView imageView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        ButterKnife.bind(this);
+        Glide.with(this).load(R.drawable.susi_image).into(imageView);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
