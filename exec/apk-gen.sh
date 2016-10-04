b8943a8cc60c95096e2685a501324cff63e24c03
Harshit Dwivedi <harshithdwivedi@gmail.com>
2016-10-04 18:33:46 +0530

    Update apk-gen.sh

--- exec/apk-gen.sh
+++ exec/apk-gen.sh
@@ -9,7 +9,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/susi_
     exit 0
 fi
 
-git clone --quiet --branch=apk https://<GITHUB_API_KEY>@github.com/fossasia/susi_android app > /dev/null
+git clone --quiet --branch=apk https://the-dagger:$GITHUB_API_KEY@github.com/fossasia/susi_android app > /dev/null
 cp platforms/android/build/outputs/apk/android-debug.apk app/susi.apk
 cd app
 git add susi.apk
