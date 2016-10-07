2fbd7122b21813976a09a6b8b46e7f4e5dab8e36
mayank <m@example.com>
2016-10-07 23:31:09 +0530

    speech to text

--- app/src/main/java/org/fossasia/susi/ai/activities/MainActivity.java
+++ app/src/main/java/org/fossasia/susi/ai/activities/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+
                 promptSpeechInput();
             }
         });
