7ef125b72312fcb1054844b350c573b52375a061
Rajan Maurya <rajanmaurya154@gmail.com>
2016-10-11 16:36:47 +0530

    code formatted in java code style and Hard Coded Strings managed in string.xml

---
+++ app/src/main/java/org/fossasia/susi/ai/helper/Constant.java
@@ -0,0 +1,12 @@
+package org.fossasia.susi.ai.helper;
+
+/**
+ * Created by Rajan Maurya on 11/10/16.
+ */
+
+public class Constant {
+
+    public static final String IMAGE_DATA = "image_data";
+
+    public static final String ENTER_SEND = "Enter_send";
+}
