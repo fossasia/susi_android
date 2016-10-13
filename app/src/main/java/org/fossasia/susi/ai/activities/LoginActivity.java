053f0e233aefb89140bb9dba5ecc18bce15cbbf8
Saurabh Singhal <singhalsaurabh95@gmail.com>
2016-10-13 12:57:10 +0530

    fixes #132: implemented authentication system.
    Implemented Sign UP and UI screens with functionality. UI still needs to be worked upon.
    Also added a new test case to follow authentication.
    
    this also partially addresses #169. we still need to decide an accent color.

---
+++ app/src/main/java/org/fossasia/susi/ai/activities/LoginActivity.java
@@ -0,0 +1,115 @@
+package org.fossasia.susi.ai.activities;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.fossasia.susi.ai.R;
+import org.fossasia.susi.ai.helper.Constant;
+import org.fossasia.susi.ai.helper.CredentialHelper;
+import org.fossasia.susi.ai.helper.PrefManager;
+import org.fossasia.susi.ai.rest.ClientBuilder;
+import org.fossasia.susi.ai.rest.model.LoginResponse;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.email)
+    TextInputLayout email;
+    @BindView(R.id.password)
+    TextInputLayout password;
+    @BindView(R.id.log_in)
+    Button logIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkIfTokenPresent();
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.log_in)
+    void logIn() {
+        if (CredentialHelper.checkIfEmpty(email, this) | CredentialHelper.checkIfEmpty(password, this)) {
+            return;
+        }
+        if (!CredentialHelper.isEmailValid(email.getEditText().getText().toString())) {
+            email.setError("Invalid email");
+            return;
+        }
+        email.setError(null);
+        if (!CredentialHelper.checkPasswordValid(password, this)) {
+            return;
+        }
+        logIn.setEnabled(false);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging in...");
+        progressDialog.show();
+        final Call<LoginResponse> authResponseCall = new ClientBuilder().getSusiApi()
+                .login(email.getEditText().getText().toString(),
+                        password.getEditText().getText().toString());
+        progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                authResponseCall.cancel();
+                logIn.setEnabled(true);
+            }
+        });
+        authResponseCall.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Toast.makeText(LoginActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                    //Save token and expiry date.
+                    PrefManager.putString(Constant.ACCESS_TOKEN, response.body().getAccessToken());
+                    long validity = System.currentTimeMillis() + response.body().getValidSeconds() * 1000;
+                    PrefManager.putLong(Constant.TOKEN_VALIDITY, validity);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                } else
+                    Toast.makeText(LoginActivity.this, response.message(), Toast.LENGTH_SHORT).show();
+                logIn.setEnabled(true);
+                progressDialog.dismiss();
+
+
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                t.printStackTrace();
+                Toast.makeText(LoginActivity.this, "Please check your internet.", Toast.LENGTH_SHORT).show();
+                logIn.setEnabled(true);
+                progressDialog.dismiss();
+            }
+        });
+    }
+
+    @OnClick(R.id.sign_up)
+    void openSignUp() {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    private void checkIfTokenPresent() {
+        if (PrefManager.getToken() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
