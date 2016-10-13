05071e4884a25df66c8abd0f758e87e50dab5f89
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2016-10-13 21:39:29 +0530

    Fixes #39, Merged with Login test and added 2 new test cases for Main Activity.
    Fixes #39, added 2 new test cases for Main Activity and added travis yml entry as suggested
    
    Fixes #39, Squashed.. added 2 more test cases to main activity and entry to travis yml. 100% success using connectedCheck on emulator and device.
    
    Fixes #39, added 2 new test cases for Main Activity and removed travis yml entry from last commit
    
    Fixes #39, added 2 new test cases for Main Activity and removed travis yml entry from last commit
    
    Fixes #39 squashed commits, commented YML entry added in last commit

--- app/src/androidTest/java/org/fossasia/susi/ai/TestMainActivity.java
+++ app/src/androidTest/java/org/fossasia/susi/ai/TestMainActivity.java
@@ -1,22 +1,33 @@
 package org.fossasia.susi.ai;
 
-import android.support.test.filters.MediumTest;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.WindowManager;
 
 import org.fossasia.susi.ai.activities.MainActivity;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
+import java.io.IOException;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressKey;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 /**
@@ -24,8 +35,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
  */
 
 @RunWith(AndroidJUnit4.class)
-@MediumTest
-public class TestMainActivity   {
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestMainActivity {
 
     private static final String TAG = TestMainActivity.class.getSimpleName();
 
@@ -33,7 +45,9 @@ public class TestMainActivity   {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
-    public void unlockScreen() {
+    public void unlockScreen() throws IOException, InterruptedException {
+        Log.d(TAG,"running unlockScreen..");
+
         final MainActivity activity = mActivityRule.getActivity();
         Runnable wakeUpDevice = new Runnable() {
             public void run() {
@@ -45,12 +59,13 @@ public class TestMainActivity   {
         activity.runOnUiThread(wakeUpDevice);
     }
 
+
     /**
      * Test activity_main items visibility on launch of app
      */
     @Test
-    public void testUIViewsPresenceOnLoad() {
-        Log.d(TAG,"running testUIViewsPresenceOnLoad..");
+    public void test01_UIViewsPresenceOnLoad() {
+        Log.d(TAG,"running test01_UIViewsPresenceOnLoad..");
 
         // checks if recycler view is present
         onView(withId(R.id.rv_chat_feed)).check(matches(isDisplayed()));
@@ -73,8 +88,8 @@ public class TestMainActivity   {
      * Test action bar items visibility
      */
     @Test
-    public void testActionBarViewsPresenceOnLoad() {
-        Log.d(TAG,"running testActionBarViewsPresenceOnLoad..");
+    public void test02_ActionBarViewsPresenceOnLoad() {
+        Log.d(TAG,"running test02_ActionBarViewsPresenceOnLoad..");
 
         // checks if search button is present
         onView(withId(R.id.action_search)).check(matches(isDisplayed()));
@@ -90,6 +105,69 @@ public class TestMainActivity   {
 
         // checks if wallpaper setting menu item is not present
         onView(withId(R.id.wall_settings)).check(doesNotExist());
+
+        // checks if log out setting menu item is not present
+        onView(withId(R.id.action_logout)).check(doesNotExist());
+
+    }
+
+    /**
+     * Test overflow items visibility on overflow icon click
+     */
+    @Test
+    public void test03_OverflowMenuItemsPresenceOnIconClick() {
+        Log.d(TAG,"running test03_OverflowMenuItemsPresenceOnIconClick..");
+
+        // open overflow menu
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        // checks if setting menu item is present
+        onView(withText(R.string.action_settings)).check(matches(isDisplayed()));
+
+        // checks if wallpaper setting menu item is present
+        onView(withText(R.string.action_wall_settings)).check(matches(isDisplayed()));
+
+        // checks if log out setting menu item is present
+        onView(withText(R.string.action_log_out)).check(matches(isDisplayed()));
+
+    }
+
+    /**
+     * Test action bar items visibility on Search click
+     */
+    @Test
+    public void test04_SearchButtonClickUIChanges() {
+        Log.d(TAG,"running test04_SearchButtonClickUIChanges..");
+
+        // enter text to chat
+        onView(withId(R.id.et_message)).perform(click()).perform(typeText("Hi! I am Unit Test"));
+
+        // click send button
+        onView(withId(R.id.btnSpeak)).perform(click());
+
+        // checks if search button is present
+        onView(withId(R.id.action_search)).check(matches(isDisplayed()));
+
+        // get the fab button and perform click operation
+        onView(withId(R.id.action_search)).perform(click());
+
+        // enter text to search
+        onView(withId(android.support.design.R.id.search_src_text)).perform(typeText("Uni"), pressKey(KeyEvent.KEYCODE_ENTER));
+
+        // checks if down angle button is present
+        onView(withId(R.id.down_angle)).check(matches(isDisplayed()));
+
+        // checks if up angle button is present
+        onView(withId(R.id.up_angle)).check(matches(isDisplayed()));
+
+        // checks if setting menu item is not present
+        onView(withId(R.id.action_settings)).check(doesNotExist());
+
+        // checks if wallpaper setting menu item is not present
+        onView(withId(R.id.wall_settings)).check(doesNotExist());
+
+        // checks if log out setting menu item is not present
+        onView(withId(R.id.action_logout)).check(doesNotExist());
 
     }
 
