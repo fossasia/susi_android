5567880d45486d07cfbb55dfa6b699c8bb137358
betterclever <paliwal.pranjal83@gmail.com>
2016-10-17 07:59:43 +0530

    Fixes #191: Copy and Delete feature in Link Messages is not working

--- app/src/main/java/org/fossasia/susi/ai/adapters/viewHolders/PieChartViewHolder.java
+++ app/src/main/java/org/fossasia/susi/ai/adapters/viewHolders/PieChartViewHolder.java
@@ -10,9 +10,12 @@ import org.fossasia.susi.ai.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import me.himanshusoni.chatmessageview.ChatMessageView;
 
 public class PieChartViewHolder extends RecyclerView.ViewHolder {
 
+    @BindView(R.id.chatMessageView)
+    public ChatMessageView chatMessageView;
     @BindView(R.id.text)
     public TextView chatTextView;
     @BindView(R.id.piechart)
