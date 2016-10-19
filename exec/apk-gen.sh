1a79810b113b428aa99a3e142ace835e84ff2d72
Harshit Dwivedi <harshithdwivedi@gmail.com>
2016-10-20 00:33:15 +0530

    Update apk-gen.sh

--- exec/apk-gen.sh
+++ exec/apk-gen.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 set -e
-
+cd ${HOME}
 git config --global user.name "the-dagger"
 git config --global user.email "harshithdwivedi@gmail.com"
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/susi_android" ]; then
-    echo "No push. Exiting."
-    exit 0
-fi
-
+ls
 git clone --quiet --branch=apk https://the-dagger:$GITHUB_API_KEY@github.com/fossasia/susi_android apk > /dev/null
-cp /home/travis/build/fossasia/susi_android/app/build/outputs/apk/app-debug.apk apk/susi-debug.apk
-cp /home/travis/build/fossasia/susi_android/app/build/outputs/apk/app-release-unsigned.apk apk/susi-release.apk
+ls
+cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/ susi_android/apk/susi-debug.apk
 cd apk
 git add .
 git commit -m "[Travis CI] Update Susi Apk"
