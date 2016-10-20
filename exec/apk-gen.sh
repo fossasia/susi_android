51a16fefb31662429993eb804c4299673b83dd52
Harshit Dwivedi <harshithdwivedi@gmail.com>
2016-10-20 15:25:54 +0530

    Update apk-gen.sh

--- exec/apk-gen.sh
+++ exec/apk-gen.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
-cd ${HOME}
+
 git config --global user.name "the-dagger"
 git config --global user.email "harshithdwivedi@gmail.com"
-ls
+
 git clone --quiet --branch=apk https://the-dagger:$GITHUB_API_KEY@github.com/fossasia/susi_android apk > /dev/null
 ls
-cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/ susi_android/apk/susi-debug.apk
+cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-debug.apk apk/susi-debug.apk
+cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-release-unsigned.apk apk/susi-release.apk
 cd apk
 git add .
-git commit -m "[Travis CI] Update Susi Apk"
+git commit -m "[Circle CI] Update Susi Apk"
 git push origin apk --quiet > /dev/null
