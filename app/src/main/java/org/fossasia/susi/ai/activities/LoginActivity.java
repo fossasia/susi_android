322000927c1ce98c0670f9a4ea57fd503a14586f
Chintan Rathod <rathodchintan@users.noreply.github.com>
2016-10-27 22:09:27 +0530

    Add butterknife to signup
    Add butterknife clicking to signup. Remove textview & click listener.

--- app/src/main/java/org/fossasia/susi/ai/activities/LoginActivity.java
+++ app/src/main/java/org/fossasia/susi/ai/activities/LoginActivity.java
@@ -34,20 +34,18 @@ public class LoginActivity extends AppCompatActivity {
     TextInputLayout password;
     @BindView(R.id.log_in)
     Button logIn;
-    TextView signup;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        signup=(TextView)findViewById(R.id.sign_up);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
+    
+    @OnClick(R.id.sign_up)
+    void signUp() {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
     }
 
     @OnClick(R.id.log_in)
