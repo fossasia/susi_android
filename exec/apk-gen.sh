34d4bc30aa418afbde0132a52bbac25b11ac2e2f
Mayank <mayank.trp48@gmail.com>
2017-01-01 17:45:08 +0530

    Fixes #448:Issue with APK downloading resolved.

--- exec/apk-gen.sh
+++ exec/apk-gen.sh
@@ -6,8 +6,8 @@ git config --global user.email "harshithdwivedi@gmail.com"
 
 git clone --quiet --branch=apk https://the-dagger:$GITHUB_API_KEY@github.com/fossasia/susi_android apk > /dev/null
 ls
-cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-x86-debug.apk apk/susi-debug.apk
-cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-x86-release-unsigned.apk apk/susi-release.apk
+cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-debug.apk apk/susi-debug.apk
+cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-release-unsigned.apk apk/susi-release.apk
 cd apk
 git add .
 git commit -m "[Circle CI] Update Susi Apk"
