f1a67d57e9d5f2d77f71cf6dca00169e759fcb4f
betterclever <paliwal.pranjal83@gmail.com>
2017-01-05 13:07:10 +0530

    Circle Fix for Failing Build after WebLink Commit

---
+++ app/src/main/java/org/fossasia/susi/ai/model/WebLink.java
@@ -0,0 +1,55 @@
+package org.fossasia.susi.ai.model;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by betterclever on 05/01/17.
+ */
+
+public class WebLink extends RealmObject {
+    private String url;
+    private String headline;
+    private String body;
+    private String imageURL;
+
+    public WebLink(){}
+
+    public WebLink(String url, String headline, String body, String imageUrl) {
+        this.url = url;
+        this.headline = headline;
+        this.body = body;
+        this.imageURL = imageUrl;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+}
