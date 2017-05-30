9494464e3d4cb3557af6eef50f2c208b82a17833
Mayank <mayank.trp48@gmail.com>
2017-05-30 11:36:28 +0530

    Table suppprt

---
+++ app/src/main/java/org/fossasia/susi/ai/adapters/viewholders/TableViewHolder.java
@@ -0,0 +1,8 @@
+package org.fossasia.susi.ai.adapters.viewholders;
+
+/**
+ * Created by mayanktripathi on 26/05/17.
+ */
+
+public class TableViewHolder {
+}
---
+++ app/src/main/java/org/fossasia/susi/ai/model/TableModel.java
@@ -0,0 +1,8 @@
+package org.fossasia.susi.ai.model;
+
+/**
+ * Created by mayanktripathi on 27/05/17.
+ */
+
+public class TableModel {
+}
