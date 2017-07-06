55474f69e0b68761acb659b3017a398bb192cf7b
amitiwary999 <tiwarya9@gmail.com>
2017-07-06 16:45:07 +0530

    Feat: change forgotpassword activity architecture to mvp using kotlin
    Feat: change forgotpassword activity architecture to mvp using kotlin
    
    Feat: change forgotpassword activity architecture to mvp using kotlin
    
    resolve codacy issue
    
    Feat : change forgotpassword to mvp
    
    feat : change forgotpassword architecture to mvp
    
    updated
    
    updated
    
    updated
    
    updated

--- app/src/main/java/org/fossasia/susi/ai/login/LoginActivity.kt
+++ app/src/main/java/org/fossasia/susi/ai/login/LoginActivity.kt
@@ -12,8 +12,8 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 import org.fossasia.susi.ai.R
 import org.fossasia.susi.ai.signup.SignUpActivity
-import org.fossasia.susi.ai.activities.ForgotPasswordActivity
 import org.fossasia.susi.ai.activities.MainActivity
+import org.fossasia.susi.ai.forgotPassword.ForgotPasswordActivity
 import org.fossasia.susi.ai.helper.AlertboxHelper
 import org.fossasia.susi.ai.helper.Constant
 
