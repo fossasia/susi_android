50ec521c018e616da1b28d8710d2e259b3bf057a
Mukuntha N S <muks14x@gmail.com>
2017-09-28 13:15:38 +0530

    fix: Backbutton press in SkillsActivity
    Fixes #995

--- app/src/main/java/org/fossasia/susi/ai/skills/skilldetails/SkillDetailsFragment.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/skilldetails/SkillDetailsFragment.kt
@@ -1,6 +1,6 @@
 package org.fossasia.susi.ai.skills.skilldetails
 
-import android.app.Fragment
+import android.support.v4.app.Fragment
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
