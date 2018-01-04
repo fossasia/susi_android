d7591aa755f283fc7b0948a7cef1ee0676a548d9
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-01-04 08:32:02 +0530

    snap helpers implemented for SUSI.AI Skills fragment adapters

--- app/src/main/java/org/fossasia/susi/ai/skills/skilllisting/adapters/recycleradapters/SkillGroupAdapter.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/skilllisting/adapters/recycleradapters/SkillGroupAdapter.kt
@@ -3,11 +3,14 @@ package org.fossasia.susi.ai.skills.skilllisting.adapters.recycleradapters
 import android.content.Context
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SnapHelper
 import android.view.ViewGroup
 import org.fossasia.susi.ai.rest.responses.susi.SkillData
 import org.fossasia.susi.ai.skills.skilllisting.adapters.viewholders.GroupViewHolder
 import android.view.LayoutInflater
+import kotlinx.android.synthetic.main.fragment_skill_listing.*
 import org.fossasia.susi.ai.R
+import org.fossasia.susi.ai.helper.StartSnapHelper
 
 /**
  *
@@ -16,13 +19,19 @@ import org.fossasia.susi.ai.R
 class SkillGroupAdapter(val context: Context, val skills: ArrayList<Pair<String, Map<String, SkillData>>>)
         :RecyclerView.Adapter<GroupViewHolder>() {
 
+    lateinit var skillAdapterSnapHelper : SnapHelper
+
     override fun onBindViewHolder(holder: GroupViewHolder?, position: Int) {
         if(skills[position].first != null)
             holder?.groupName?.text = skills[position].first
+
+        skillAdapterSnapHelper = StartSnapHelper()
         holder?.skillList?.setHasFixedSize(true)
         val mLayoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         holder?.skillList?.layoutManager = mLayoutManager
         holder?.skillList?.adapter = SkillListAdapter(context, skills[position])
+        holder?.skillList?.setOnFlingListener(null)
+        skillAdapterSnapHelper.attachToRecyclerView(holder?.skillList)
     }
 
     override fun getItemCount(): Int {
