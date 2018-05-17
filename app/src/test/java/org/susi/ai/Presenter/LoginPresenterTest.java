fdeb3a5d29446036328534baca2999eb8b2c5c49
batbrain7 <kumar.mohit983@gmail.com>
2018-05-17 19:43:47 +0530

    Unit tests added

--- app/src/test/java/org/susi/ai/Presenter/LoginPresenterTest.java
+++ app/src/test/java/org/susi/ai/Presenter/LoginPresenterTest.java
@@ -1,13 +1,10 @@
 package org.fossasia.susi.ai.Presenter;
 
-import org.fossasia.susi.ai.data.LoginModel;
 import org.fossasia.susi.ai.data.UtilModel;
 import org.fossasia.susi.ai.data.contract.ILoginModel;
 import org.fossasia.susi.ai.login.LoginActivity;
 import org.fossasia.susi.ai.login.LoginPresenter;
-import org.fossasia.susi.ai.login.contract.ILoginPresenter;
 import org.fossasia.susi.ai.login.contract.ILoginView;
-import org.fossasia.susi.ai.rest.interceptors.TokenInterceptor;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,17 +14,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-
-import java.util.prefs.Preferences;
-
 import static org.mockito.ArgumentMatchers.any;
 
 @RunWith(JUnit4.class)
 public class LoginPresenterTest {
 
     @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
-    @Mock private Preferences sharedPreferences;
-    @Mock private LoginModel loginModel;
+    @Mock private ILoginModel loginModel;
     @Mock private ILoginView iLoginView;
     @Mock private UtilModel model;
     @Mock private LoginActivity loginActivity;
@@ -37,12 +30,13 @@ public class LoginPresenterTest {
     private static final String EMAIL = "test";
     private static final String PASSWORD = "test";
     private static final String URL = "test";
-    private static final boolean SERVERCONNECTED = false;
+    private static final boolean SERVERCONNECTED = true;
 
 
     @Before
     public void setUp() {
         loginPresenter = new LoginPresenter(loginActivity);
+        loginPresenter.getMessage();
         loginPresenter.login(EMAIL,PASSWORD,SERVERCONNECTED,URL);
 
     }
@@ -53,7 +47,7 @@ public class LoginPresenterTest {
 
         loginPresenter.onAttach(iLoginView);
 
-        Mockito.verify(loginActivity).onLoginSuccess("MESSAGE");
+        Mockito.verify(iLoginView).onLoginSuccess((String) any());
     }
 
     @Test
@@ -62,8 +56,7 @@ public class LoginPresenterTest {
 
         loginPresenter.onAttach(iLoginView);
 
-        Mockito.verify(loginActivity, Mockito.never()).onLoginSuccess("MESSAGE");
+        Mockito.verify(iLoginView, Mockito.never()).onLoginSuccess((String) any());
     }
-
 
 }
