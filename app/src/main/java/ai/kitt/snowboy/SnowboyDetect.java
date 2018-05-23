689028a9b36b6ecd1983fc80976af1794ffb973a
Mohit Kumar <mohitkumar2k15@dtu.ac.in>
2018-05-23 21:31:55 +0530

    refactor: Remove the unused code (#1221)

--- app/src/main/java/ai/kitt/snowboy/SnowboyDetect.java
+++ app/src/main/java/ai/kitt/snowboy/SnowboyDetect.java
@@ -17,10 +17,6 @@ public class SnowboyDetect {
     swigCPtr = cPtr;
   }
 
-  protected static long getCPtr(SnowboyDetect obj) {
-    return (obj == null) ? 0 : obj.swigCPtr;
-  }
-
   protected void finalize() {
     delete();
   }
@@ -39,76 +35,15 @@ public class SnowboyDetect {
     this(snowboyJNI.new_SnowboyDetect(resource_filename, model_str), true);
   }
 
-  public boolean reset() {
-    return snowboyJNI.SnowboyDetect_Reset(swigCPtr, this);
-  }
-
-  public int runDetection(String data, boolean is_end) {
-    return snowboyJNI.SnowboyDetect_RunDetection__SWIG_0(swigCPtr, this, data, is_end);
-  }
-
-  public int runDetection(String data) {
-    return snowboyJNI.SnowboyDetect_RunDetection__SWIG_1(swigCPtr, this, data);
-  }
-
-  public int runDetection(float[] data, int array_length, boolean is_end) {
-    return snowboyJNI.SnowboyDetect_RunDetection__SWIG_2(swigCPtr, this, data, array_length, is_end);
-  }
-
-  public int runDetection(float[] data, int array_length) {
-    return snowboyJNI.SnowboyDetect_RunDetection__SWIG_3(swigCPtr, this, data, array_length);
-  }
-
-  public int runDetection(short[] data, int array_length, boolean is_end) {
-    return snowboyJNI.SnowboyDetect_RunDetection__SWIG_4(swigCPtr, this, data, array_length, is_end);
-  }
-
   public int runDetection(short[] data, int array_length) {
     return snowboyJNI.SnowboyDetect_RunDetection__SWIG_5(swigCPtr, this, data, array_length);
-  }
-
-  public int runDetection(int[] data, int array_length, boolean is_end) {
-    return snowboyJNI.SnowboyDetect_RunDetection__SWIG_6(swigCPtr, this, data, array_length, is_end);
-  }
-
-  public int runDetection(int[] data, int array_length) {
-    return snowboyJNI.SnowboyDetect_RunDetection__SWIG_7(swigCPtr, this, data, array_length);
   }
 
   public void setSensitivity(String sensitivity_str) {
     snowboyJNI.SnowboyDetect_SetSensitivity(swigCPtr, this, sensitivity_str);
   }
 
-  public String getSensitivity() {
-    return snowboyJNI.SnowboyDetect_GetSensitivity(swigCPtr, this);
-  }
-
-  public void setAudioGain(float audio_gain) {
-    snowboyJNI.SnowboyDetect_SetAudioGain(swigCPtr, this, audio_gain);
-  }
-
-  public void updateModel() {
-    snowboyJNI.SnowboyDetect_UpdateModel(swigCPtr, this);
-  }
-
-  public int numHotwords() {
-    return snowboyJNI.SnowboyDetect_NumHotwords(swigCPtr, this);
-  }
-
   public void applyFrontend(boolean apply_frontend) {
     snowboyJNI.SnowboyDetect_ApplyFrontend(swigCPtr, this, apply_frontend);
   }
-
-  public int sampleRate() {
-    return snowboyJNI.SnowboyDetect_SampleRate(swigCPtr, this);
-  }
-
-  public int numChannels() {
-    return snowboyJNI.SnowboyDetect_NumChannels(swigCPtr, this);
-  }
-
-  public int bitsPerSample() {
-    return snowboyJNI.SnowboyDetect_BitsPerSample(swigCPtr, this);
-  }
-
 }
