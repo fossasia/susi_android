85b2edc33d6338b30ae8e07068d369e57ddfd2df
CodeJack <arjunhegde372@gmail.com>
2018-05-23 07:26:15 +0530

    Fixed crash #1279

--- app/src/main/java/org/fossasia/susi/ai/chat/ChatActivity.kt
+++ app/src/main/java/org/fossasia/susi/ai/chat/ChatActivity.kt
@@ -386,11 +386,11 @@ class ChatActivity: AppCompatActivity(), IChatView {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
-    fun scrollToEnd() {
+    fun scrollToEnd(view: View) {
         rv_chat_feed.smoothScrollToPosition(rv_chat_feed.adapter.itemCount - 1)
     }
 
-    fun openSettings() {
+    fun openSettings(view: View) {
         val i = Intent(this, SkillsActivity::class.java)
         startActivity(i)
         finish()
