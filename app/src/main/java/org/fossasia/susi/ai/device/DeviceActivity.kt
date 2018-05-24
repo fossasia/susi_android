ad043d8d12ad95b7d11feb1a96108fa90c2012c0
the-dagger <harshithdwivedi@gmail.com>
2018-05-24 13:17:52 +0530

    Added wifi choosing option

--- app/src/main/java/org/fossasia/susi/ai/device/DeviceActivity.kt
+++ app/src/main/java/org/fossasia/susi/ai/device/DeviceActivity.kt
@@ -2,13 +2,23 @@ package org.fossasia.susi.ai.device
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.view.MenuItem
+import android.view.View
+import org.fossasia.susi.ai.BuildConfig
 import org.fossasia.susi.ai.R
-import android.support.v4.app.NavUtils
+import org.fossasia.susi.ai.device.contract.IDeviceView
+import android.content.Intent
+import android.content.ComponentName
 
 
 
-class DeviceActivity : AppCompatActivity() {
+
+class DeviceActivity : AppCompatActivity(),IDeviceView {
+
+    val TAG: String = DeviceActivity::class.java.name
+    lateinit var devicePresenter: DevicePresenter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +41,38 @@ class DeviceActivity : AppCompatActivity() {
     override fun onBackPressed() {
         overridePendingTransition(R.anim.trans_right_in, R.anim.trans_right_out)
         super.onBackPressed()
+    }
+
+    override fun onDeviceConnectedSuccess() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showProgress() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onDeviceConnectionError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun chooseWifi(view: View) {
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(R.string.device_hosted_connection)
+        builder.setMessage(R.string.choose_wifi)
+        builder.setPositiveButton("OK"){dialog, which ->
+            val intent = Intent(Intent.ACTION_MAIN, null)
+            intent.addCategory(Intent.CATEGORY_LAUNCHER)
+            val cn = ComponentName("com.android.settings", "com.android.settings.wifi.WifiSettings")
+            intent.component = cn
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+
+        builder.setNegativeButton("CANCEL"){dialog,which ->
+        }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
