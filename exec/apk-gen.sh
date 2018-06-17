ebf9af2a1cc471c7bb3fb3645d72b20414a56ce7
batbrain7 <kumar.mohit983@gmail.com>
2018-06-17 17:09:47 +0530

    Update the paths everywhere

--- exec/apk-gen.sh
+++ exec/apk-gen.sh
@@ -24,7 +24,8 @@ then
 		echo "Push to master branch detected, signing the app..."
 		\cp app-fdroid-release-unsigned.apk app-fdroid-release-unaligned.apk
 		\cp app-playStore-release-unsigned.apk app-playStore-release-unaligned.apk
-		jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../exec/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-release-unaligned.apk $ALIAS
+		jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../exec/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-fdroid-release-unaligned.apk $ALIAS
+		jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../exec/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-playStore-release-unaligned.apk $ALIAS
 		${ANDROID_HOME}/build-tools/27.0.3/zipalign -vfp 4 app-fdroid-release-unaligned.apk app-fdroid-release.apk
 		${ANDROID_HOME}/build-tools/27.0.3/zipalign -vfp 4 app-playStore-release-unaligned.apk app-playStore-release.apk
 	fi
@@ -39,12 +40,14 @@ then
 
 	git push origin apk --force --quiet > /dev/null
 
-	curl https://$APPETIZE_API_TOKEN@api.appetize.io/v1/apps/mbpprq4xj92c119j7nxdhttjm0 -H 'Content-Type: application/json' -d '{"url":"https://github.com/fossasia/susi_android/raw/apk/app-debug.apk", "note": "Update SUSI Preview"}'
+	curl https://$APPETIZE_API_TOKEN@api.appetize.io/v1/apps/mbpprq4xj92c119j7nxdhttjm0 -H 'Content-Type: application/json' -d '{"url":"https://github.com/fossasia/susi_android/raw/apk/app-fdroid-debug.apk", "note": "Update SUSI Preview"}'
+	curl https://$APPETIZE_API_TOKEN@api.appetize.io/v1/apps/mbpprq4xj92c119j7nxdhttjm0 -H 'Content-Type: application/json' -d '{"url":"https://github.com/fossasia/susi_android/raw/apk/app-playStore-debug.apk", "note": "Update SUSI Preview"}'
 
 	# Publish App to Play Store
 	if [ "$CIRCLE_BRANCH" == "$PUBLISH_BRANCH" ]; then
 		echo "Publishing app to Play Store"
 		gem install fastlane
-		fastlane supply --apk app-release.apk --track alpha --json_key ../exec/fastlane.json --package_name $PACKAGE_NAME
+		fastlane supply --apk app-fdroid-release.apk --track alpha --json_key ../exec/fastlane.json --package_name $PACKAGE_NAME
+		fastlane supply --apk app-playStore-release.apk --track alpha --json_key ../exec/fastlane.json --package_name $PACKAGE_NAME
 	fi
 fi
