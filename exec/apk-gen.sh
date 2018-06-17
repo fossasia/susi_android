a36606a5dffeaceb91b44baff1d229df29342fed
Mohit Kumar <kumar.mohit983@gmail.com>
2018-06-17 17:53:35 +0530

    chore: Add the release apk paths in apk gen script (#1409)
    * Add release path
    
    * Updated paths
    
    * Update the paths everywhere
    
    * Remove fdroid upload

--- exec/apk-gen.sh
+++ exec/apk-gen.sh
@@ -22,9 +22,9 @@ then
 	# Signing App
 	if [ "$CIRCLE_BRANCH" == "$PUBLISH_BRANCH" ]; then
 		echo "Push to master branch detected, signing the app..."
-		\cp app-release-unsigned.apk app-release-unaligned.apk
-		jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../exec/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-release-unaligned.apk $ALIAS
-		${ANDROID_HOME}/build-tools/27.0.3/zipalign -vfp 4 app-release-unaligned.apk app-release.apk
+		\cp app-playStore-release-unsigned.apk app-playStore-release-unaligned.apk
+		jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../exec/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-playStore-release-unaligned.apk $ALIAS
+		${ANDROID_HOME}/build-tools/27.0.3/zipalign -vfp 4 app-playStore-release-unaligned.apk app-playStore-release.apk
 	fi
 
 	git checkout --orphan workaround
@@ -37,12 +37,12 @@ then
 
 	git push origin apk --force --quiet > /dev/null
 
-	curl https://$APPETIZE_API_TOKEN@api.appetize.io/v1/apps/mbpprq4xj92c119j7nxdhttjm0 -H 'Content-Type: application/json' -d '{"url":"https://github.com/fossasia/susi_android/raw/apk/app-debug.apk", "note": "Update SUSI Preview"}'
+	curl https://$APPETIZE_API_TOKEN@api.appetize.io/v1/apps/mbpprq4xj92c119j7nxdhttjm0 -H 'Content-Type: application/json' -d '{"url":"https://github.com/fossasia/susi_android/raw/apk/app-playStore-debug.apk", "note": "Update SUSI Preview"}'
 
 	# Publish App to Play Store
 	if [ "$CIRCLE_BRANCH" == "$PUBLISH_BRANCH" ]; then
 		echo "Publishing app to Play Store"
 		gem install fastlane
-		fastlane supply --apk app-release.apk --track alpha --json_key ../exec/fastlane.json --package_name $PACKAGE_NAME
+		fastlane supply --apk app-playStore-release.apk --track alpha --json_key ../exec/fastlane.json --package_name $PACKAGE_NAME
 	fi
 fi
