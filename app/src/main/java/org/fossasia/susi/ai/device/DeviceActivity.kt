841b4adaafa719832c10c3822e93fc5b1be27c82
batbrain7 <kumar.mohit983@gmail.com>
2018-06-21 12:51:40 +0530

    Scan nearby wifi points

--- app/src/main/java/org/fossasia/susi/ai/device/DeviceActivity.kt
+++ app/src/main/java/org/fossasia/susi/ai/device/DeviceActivity.kt
@@ -1,13 +1,16 @@
 package org.fossasia.susi.ai.device
 
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
@@ -17,9 +20,7 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_device.*
 import org.fossasia.susi.ai.R
 import org.fossasia.susi.ai.device.contract.IDeviceView
-import org.fossasia.susi.ai.device.DeviceActivity.WifiReceiver
-
-
+import timber.log.Timber
 
 
 class DeviceActivity : AppCompatActivity(), IDeviceView {
@@ -27,45 +28,36 @@ class DeviceActivity : AppCompatActivity(), IDeviceView {
     lateinit var devicePresenter: DevicePresenter
     lateinit var mainWifi: WifiManager
     lateinit var receiverWifi: WifiReceiver
+    lateinit var filter: IntentFilter
+    private val PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         overridePendingTransition(R.anim.trans_left_in, R.anim.trans_left_out)
         setContentView(R.layout.activity_device)
 
-
+        filter = IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
         showProgress()
-        mainWifi = getApplicationContext().getSystemService(Context.WIFI_SERVICE) as WifiManager
-
-        receiverWifi = WifiReceiver();
-        registerReceiver(receiverWifi, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        if(mainWifi.isWifiEnabled == false) {
-            mainWifi.setWifiEnabled(true);
-        }
-        mainWifi.startScan()
-
         devicePresenter = DevicePresenter(this)
         devicePresenter.onAttach(this)
-
-        //startScan()
+        startScan()
     }
 
     private fun startScan() {
         noDeviceFound.visibility = View.GONE
         deviceTutorial.visibility = View.GONE
-
-        Handler().postDelayed({
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(Array<String>(1,{Manifest.permission.ACCESS_COARSE_LOCATION}),
+                    PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        }else{
             mainWifi = application.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
             receiverWifi = WifiReceiver()
-            registerReceiver(receiverWifi, IntentFilter(
-                    WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+            registerReceiver(receiverWifi,filter)
             mainWifi.startScan()
-            startScan()
-        }, 1000)
-
-        scanDevice.visibility = View.VISIBLE
-        scanProgress.visibility = View.VISIBLE
+            //do something, permission was previously granted; or legacy device
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -91,6 +83,8 @@ class DeviceActivity : AppCompatActivity(), IDeviceView {
 
     override fun showProgress() {
         scanProgress.getIndeterminateDrawable().setColorFilter(Color.parseColor("#ffc100"), android.graphics.PorterDuff.Mode.MULTIPLY)
+        scanDevice.visibility = View.VISIBLE
+        scanProgress.visibility = View.VISIBLE
     }
 
     override fun onDeviceConnectionError() {
@@ -109,20 +103,50 @@ class DeviceActivity : AppCompatActivity(), IDeviceView {
         startScan()
     }
 
+    override fun onPause() {
+        unregisterReceiver(receiverWifi)
+        super.onPause()
+    }
+
+    override fun onResume() {
+        registerReceiver(receiverWifi, filter)
+        super.onResume()
+    }
+
     inner class WifiReceiver: BroadcastReceiver() {
 
         override fun onReceive(p0: Context?, p1: Intent?) {
 
-            val connections = ArrayList<String>()
 
-            var sb = StringBuilder()
-            var wifiList: List<ScanResult> = ArrayList<ScanResult>()
-            wifiList = mainWifi.getScanResults()
-            for (i in wifiList.indices) {
-                connections.add(wifiList[i].SSID)
-                Toast.makeText(p0,connections.get(i),Toast.LENGTH_LONG).show()
-            }
+                Toast.makeText(p0, "Inside the app", Toast.LENGTH_LONG).show()
+
+                val connections = ArrayList<String>()
+
+                var sb = StringBuilder()
+                var wifiList: List<ScanResult> = ArrayList<ScanResult>()
+                wifiList = mainWifi.getScanResults()
+                Timber.d("size " + wifiList.size)
+                for (i in wifiList.indices) {
+                    connections.add(wifiList[i].SSID)
+                    Timber.d(connections.get(i))
+                }
         }
 
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION -> run {
+                for (i in permissions.indices) {
+                    when (permissions[i]) {
+                        Manifest.permission.ACCESS_COARSE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                          //  chatPresenter.getLocationFromLocationService()
+                            startScan()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
