e30d2149e72689d93168571ca87f16940163868e
batbrain7 <kumar.mohit983@gmail.com>
2018-06-22 20:25:12 +0530

    Remove variables from DB

--- app/src/main/java/org/fossasia/susi/ai/data/db/DatabaseRepository.kt
+++ app/src/main/java/org/fossasia/susi/ai/data/db/DatabaseRepository.kt
@@ -100,9 +100,6 @@ class DatabaseRepository : IDatabaseRepository {
                         realmDatum.description = datum.description
                         realmDatum.link = datum.link
                         realmDatum.title = datum.title
-                        realmDatum.jerseyNumber = datum.jerseyNumber
-                        realmDatum.name = datum.name
-                        realmDatum.position = datum.position
                         datumRealmList.add(realmDatum)
                     }
                     chatMessage.datumRealmList = datumRealmList
