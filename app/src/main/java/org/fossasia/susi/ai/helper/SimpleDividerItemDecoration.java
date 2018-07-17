fff3545dad6e52a73097fb81b23cc86a5ecbdd1b
arundhati24 <arundhatigupta2411@gmail.com>
2018-07-17 23:14:57 +0530

    Remove Hungarian notation

--- app/src/main/java/org/fossasia/susi/ai/helper/SimpleDividerItemDecoration.java
+++ app/src/main/java/org/fossasia/susi/ai/helper/SimpleDividerItemDecoration.java
@@ -12,30 +12,30 @@ import org.fossasia.susi.ai.R;
  * Created by arundhati24 16/07/2018.
  */
 public class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
-    private Drawable mDivider;
-    private int mDrawDividerFromIndex;
+    private Drawable divider;
+    private int drawDividerFromIndex;
 
     public SimpleDividerItemDecoration(Context context, int drawDividerFromIndex) {
-        mDivider = context.getResources().getDrawable(R.drawable.rv_divider);
-        mDrawDividerFromIndex = drawDividerFromIndex;
+        divider = context.getResources().getDrawable(R.drawable.rv_divider);
+        this.drawDividerFromIndex = drawDividerFromIndex;
     }
 
     @Override
-    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+    public void onDrawOver(Canvas canvas, RecyclerView parent, RecyclerView.State state) {
         int left = parent.getPaddingLeft();
         int right = parent.getWidth() - parent.getPaddingRight();
 
         int childCount = parent.getChildCount();
-        for (int i = mDrawDividerFromIndex; i < childCount; i++) {
+        for (int i = drawDividerFromIndex; i < childCount; i++) {
             View child = parent.getChildAt(i);
 
             RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
 
             int top = child.getBottom() + params.bottomMargin;
-            int bottom = top + mDivider.getIntrinsicHeight();
+            int bottom = top + divider.getIntrinsicHeight();
 
-            mDivider.setBounds(left, top, right, bottom);
-            mDivider.draw(c);
+            divider.setBounds(left, top, right, bottom);
+            divider.draw(canvas);
         }
     }
 }
