52f7f26d25ce19ad6409e5badcc8edae6caa4623
batbrain7 <kumar.mohit983@gmail.com>
2018-07-17 19:04:28 +0530

    codacy issue

--- app/src/main/java/org/fossasia/susi/ai/device/deviceconnect/adapters/viewholders/WifiViewHolder.java
+++ app/src/main/java/org/fossasia/susi/ai/device/deviceconnect/adapters/viewholders/WifiViewHolder.java
@@ -1,7 +1,6 @@
 package org.fossasia.susi.ai.device.deviceconnect.adapters.viewholders;
 
 import android.content.DialogInterface;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
