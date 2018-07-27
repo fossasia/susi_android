b4fc982a9ac7b2600348aec893232016ae721a5b
batbrain7 <kumar.mohit983@gmail.com>
2018-07-27 21:42:00 +0530

    chore: convert susiresponse to data class

--- app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/SusiResponse.kt
+++ app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/SusiResponse.kt
@@ -9,38 +9,38 @@ import java.util.ArrayList
  * <h1>Kotlin Data class to parse retrofit response from susi client.</h1>
  */
 
-class SusiResponse {
+data class SusiResponse(
 
-    @SerializedName("client_id")
-    @Expose
-    val clientId: String? = null
+        @SerializedName("client_id")
+        @Expose
+        val clientId: String? = null,
 
-    @SerializedName("query")
-    @Expose
-    val query: String = ""
+        @SerializedName("query")
+        @Expose
+        val query: String = "",
 
-    @SerializedName("query_date")
-    @Expose
-    val queryDate: String = ""
+        @SerializedName("query_date")
+        @Expose
+        val queryDate: String = "",
 
-    @SerializedName("answer_date")
-    @Expose
-    val answerDate: String = ""
+        @SerializedName("answer_date")
+        @Expose
+        val answerDate: String = "",
 
-    @SerializedName("answer_time")
-    @Expose
-    val answerTime: Int = 0
+        @SerializedName("answer_time")
+        @Expose
+        val answerTime: Int = 0,
 
-    @SerializedName("count")
-    @Expose
-    val count: Int = 0
+        @SerializedName("count")
+        @Expose
+        val count: Int = 0,
 
-    @SerializedName("answers")
-    @Expose
-    val answers: List<Answer> = ArrayList()
+        @SerializedName("answers")
+        @Expose
+        val answers: List<Answer> = ArrayList(),
 
-    @SerializedName("session")
-    @Expose
-    val session: Session? = null
+        @SerializedName("session")
+        @Expose
+        val session: Session? = null
 
-}
+)
