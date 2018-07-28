ad7a5604b11e0164cfb2671eedaedc7fe18f035d
batbrain7 <kumar.mohit983@gmail.com>
2018-07-29 01:04:57 +0530

    removed serialized

--- app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/SusiResponse.kt
+++ app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/SusiResponse.kt
@@ -12,35 +12,23 @@ import java.util.ArrayList
 data class SusiResponse(
 
         @SerializedName("client_id")
-        @Expose
         val clientId: String? = null,
 
-        @SerializedName("query")
-        @Expose
         val query: String = "",
 
         @SerializedName("query_date")
-        @Expose
         val queryDate: String = "",
 
         @SerializedName("answer_date")
-        @Expose
         val answerDate: String = "",
 
         @SerializedName("answer_time")
-        @Expose
         val answerTime: Int = 0,
 
-        @SerializedName("count")
-        @Expose
         val count: Int = 0,
 
-        @SerializedName("answers")
-        @Expose
         val answers: List<Answer> = ArrayList(),
 
-        @SerializedName("session")
-        @Expose
         val session: Session? = null
 
 )
