e3383bf7ea0f19ca02aa3b399ce60f1a9bb21805
batbrain7 <kumar.mohit983@gmail.com>
2018-07-29 20:13:41 +0530

    convert response classes to data

--- app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/SkillRatingResponse.kt
+++ app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/SkillRatingResponse.kt
@@ -4,8 +4,8 @@ package org.fossasia.susi.ai.rest.responses.susi
  *
  * Created by chiragw15 on 6/8/17.
  */
-class SkillRatingResponse {
-    val session: Session? = null
-    val accepted: Boolean = false
-    val message: String = ""
-}
+data class SkillRatingResponse(
+        val session: Session? = null,
+        val accepted: Boolean = false,
+        val message: String = ""
+)
--- app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/Skills.kt
+++ app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/Skills.kt
@@ -7,8 +7,7 @@ import com.google.gson.annotations.SerializedName
  *
  * Created by cc15 on 16/8/17.
  */
-class Skills {
+data class Skills (
     @SerializedName("aboutsusi")
-    @Expose
     val skillData: SkillData? = null
-}
+)
