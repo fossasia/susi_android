5477aebb56d1c70aad0c32be1c3cef145c2df2c0
batbrain7 <kumar.mohit983@gmail.com>
2018-07-29 19:34:36 +0530

    remove link

--- app/src/main/java/org/fossasia/susi/ai/helper/Utils.kt
+++ app/src/main/java/org/fossasia/susi/ai/helper/Utils.kt
@@ -3,12 +3,9 @@ package org.fossasia.susi.ai.helper
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
 import org.fossasia.susi.ai.R
-import org.fossasia.susi.ai.rest.clients.BaseUrl
 import org.fossasia.susi.ai.rest.responses.susi.SkillData
 
 object Utils {
-    private val imageLink = BaseUrl.SUSI_DEFAULT_BASE_URL + "/cms/getImage.png?"
-
     fun setSkillsImage(skillData: SkillData, imageView: ImageView) {
         Picasso.with(imageView.context)
                 .load(getImageLink(skillData))
@@ -19,7 +16,7 @@ object Utils {
     }
 
     fun getImageLink(skillData: SkillData): String {
-        return "$imageLink" + "model=" + "$skillData.model" + "&language=" + "$skillData.language" + "&group=" +
+        return "BaseUrl.SUSI_DEFAULT_BASE_URL + \"/cms/getImage.png?\"" + "model=" + "$skillData.model" + "&language=" + "$skillData.language" + "&group=" +
                 StringBuffer(skillData.group.replace(" ", "%20")).toString() + "&image=" + "$skillData.image"
     }
 
