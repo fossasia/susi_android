a4f1eaf362f02711ccfa143241d2c3f0f70c279b
batbrain7 <kumar.mohit983@gmail.com>
2018-07-30 19:06:06 +0530

    data class added

--- app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/FeedBackLogResponse.kt
+++ app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/FeedBackLogResponse.kt
@@ -1,4 +1,21 @@
 package org.fossasia.susi.ai.rest.responses.susi
 
-class FeedBackLogResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class FeedBackLogResponse(
+        val model: String? = null,
+        val group: String? = null,
+        val language: String? = null,
+        val skill: String? = null,
+        val feedback: String? = null,
+        @SerializedName("user_query")
+        val userQuery: String? = null,
+        @SerializedName("susi_reply")
+        val susiReply: String? = null,
+        @SerializedName("country_name")
+        val countryName: String? = null,
+        @SerializedName("country_code")
+        val countryCode: String? = null,
+        @SerializedName("device_type")
+        val deviceType: String? = null
+)
