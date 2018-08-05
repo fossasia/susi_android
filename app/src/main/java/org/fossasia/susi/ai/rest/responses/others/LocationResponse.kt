ccc115fa168d003c7a5c1fe11333f6663c397778
Arundhati Gupta <arundhatigupta2411@gmail.com>
2018-08-05 17:13:06 +0530

    refactor: Convert all classes that hold data to Kotlin data class (#1649)

--- app/src/main/java/org/fossasia/susi/ai/rest/responses/others/LocationResponse.kt
+++ app/src/main/java/org/fossasia/susi/ai/rest/responses/others/LocationResponse.kt
@@ -8,29 +8,13 @@ import com.google.gson.annotations.SerializedName
  * Created by chiragw15 on 6/12/16.
  */
 
-class LocationResponse {
-
-    @SerializedName("ip")
-    var ip: String? = null
-
-    @SerializedName("hostname")
-    var hostname: String? = null
-
-    @SerializedName("city")
-    var city: String? = null
-
-    @SerializedName("region")
-    var region: String? = null
-
-    @SerializedName("country")
-    var country: String? = null
-
-    @SerializedName("loc")
-    var loc: String = ""
-
-    @SerializedName("org")
-    var org: String? = null
-
-    @SerializedName("postal")
+data class LocationResponse (
+    var ip: String? = null,
+    var hostname: String? = null,
+    var city: String? = null,
+    var region: String? = null,
+    var country: String? = null,
+    var loc: String = "",
+    var org: String? = null,
     var postal: String? = null
-}
+)
--- app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/LoginResponse.kt
+++ app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/LoginResponse.kt
@@ -1,6 +1,5 @@
 package org.fossasia.susi.ai.rest.responses.susi
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /**
@@ -9,14 +8,11 @@ import com.google.gson.annotations.SerializedName
  * Created by saurabh on 12/10/16.
  */
 
-class LoginResponse {
-    @SerializedName("message")
-    val message: String? = null
-    @SerializedName("session")
-    val session: Session? = null
-    @SerializedName("valid_seconds")
-    val validSeconds: Long = 0
-    @SerializedName("access_token")
-    var accessToken: String? = null
-        internal set
-}
+data class LoginResponse(
+        val message: String? = null,
+        val session: Session? = null,
+        @SerializedName("valid_seconds")
+        val validSeconds: Long = 0,
+        @SerializedName("access_token")
+        var accessToken: String? = null
+)
