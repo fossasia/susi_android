bc7269ee62219ab00c3b706898e0b66f0009b0c0
Arundhati Gupta <arundhatigupta2411@gmail.com>
2018-10-19 13:52:51 +0530

    feat: Show the name of the user if available (#1693)
    Display the user name in place of email in the feedback section, if
    available. In case the username is not available then show the email
    as usual.

--- app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/Feedback.kt
+++ app/src/main/java/org/fossasia/susi/ai/rest/responses/susi/Feedback.kt
@@ -1,6 +1,7 @@
 package org.fossasia.susi.ai.rest.responses.susi
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 /**
@@ -12,5 +13,7 @@ data class Feedback(
         var feedback: String? = "",
         var email: String? = "",
         var timestamp: String? = "",
+        @SerializedName("user_name")
+        var userName: String = "",
         var avatar: String? = ""
 ) : Parcelable
