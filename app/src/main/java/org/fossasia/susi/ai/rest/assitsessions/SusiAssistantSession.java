a861305959cedd9c2b402ba2d10e9282ec456610
Zameel Pichen <zameelpichen@gmail.com>
2018-12-14 11:01:15 +0530

    Updated Handling Test

--- app/src/main/java/org/fossasia/susi/ai/rest/assitsessions/SusiAssistantSession.java
+++ app/src/main/java/org/fossasia/susi/ai/rest/assitsessions/SusiAssistantSession.java
@@ -1,9 +1,16 @@
 package org.fossasia.susi.ai.rest.assitsessions;
 
 import android.annotation.TargetApi;
+import android.app.assist.AssistContent;
+import android.app.assist.AssistStructure;
 import android.content.Context;
 import android.os.Build;
+import android.os.Bundle;
 import android.service.voice.VoiceInteractionSession;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @TargetApi(21)
 public class SusiAssistantSession extends VoiceInteractionSession {
@@ -12,5 +19,21 @@ public class SusiAssistantSession extends VoiceInteractionSession {
 
             super(context);
         }
+
+    @Override
+    public void onHandleAssist(Bundle data,
+                               AssistStructure structure, AssistContent content) {
+        super.onHandleAssist(data, structure, content);
+
+        if (Build.VERSION.SDK_INT>=23) {
+            // Display description as Toast
+            Toast.makeText(
+                    getContext(),
+                    "My Name is Susi",
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+
+    }
 
 }
