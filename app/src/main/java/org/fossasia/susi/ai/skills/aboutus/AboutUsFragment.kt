fdc1693b5a188f114f90a158256fd55bf948d2b3
Karan Agarwal <karan@Karans-MacBook-Pro.local>
2019-01-08 19:31:12 +0530

    fix: Show the hidden text on about-us page

--- app/src/main/java/org/fossasia/susi/ai/skills/aboutus/AboutUsFragment.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/aboutus/AboutUsFragment.kt
@@ -2,10 +2,12 @@ package org.fossasia.susi.ai.skills.aboutus
 
 
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.support.annotation.NonNull
 import android.support.customtabs.CustomTabsIntent
 import android.support.v4.app.Fragment
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
@@ -43,29 +45,61 @@ class AboutUsFragment : Fragment() {
 
     @NonNull
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        about_susi.setOnClickListener({
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            about_susi.text = Html.fromHtml(getString(R.string.susi_about), Html.FROM_HTML_OPTION_USE_CSS_COLORS) // for 24 api and more
+        } else {
+            about_susi.text = Html.fromHtml(getString(R.string.susi_about)) // or for older api
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            contributors_desc.text = Html.fromHtml(getString(R.string.susi_contributors_desc), Html.FROM_HTML_OPTION_USE_CSS_COLORS) // for 24 api and more
+        } else {
+            contributors_desc.text = Html.fromHtml(getString(R.string.susi_contributors_desc)) // or for older api
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            susi_skill_cms_desc.text = Html.fromHtml(getString(R.string.susi_skill_cms_desc), Html.FROM_HTML_OPTION_USE_CSS_COLORS) // for 24 api and more
+        } else {
+            susi_skill_cms_desc.text = Html.fromHtml(getString(R.string.susi_skill_cms_desc)) // or for older api
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            susi_license_info_desc.text = Html.fromHtml(getString(R.string.susi_license_information_desc), Html.FROM_HTML_OPTION_USE_CSS_COLORS) // for 24 api and more
+        } else {
+            susi_license_info_desc.text = Html.fromHtml(getString(R.string.susi_license_information_desc)) // or for older api
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            susi_report_issues_desc.text = Html.fromHtml(getString(R.string.susi_report_issues_desc), Html.FROM_HTML_OPTION_USE_CSS_COLORS) // for 24 api and more
+        } else {
+            susi_report_issues_desc.text = Html.fromHtml(getString(R.string.susi_report_issues_desc)) // or for older api
+        }
+
+
+        about_susi.setOnClickListener{
             val uri = Uri.parse(getString(R.string.url_about_susi))
             launchCustomtTab(uri)
-        })
+        }
 
-        contributors_desc.setOnClickListener({
+        contributors_desc.setOnClickListener{
             val uri = Uri.parse(getString(R.string.url_susi_contributors))
             launchCustomtTab(uri)
-        })
+        }
 
-        susi_skill_cms_desc.setOnClickListener({
+        susi_skill_cms_desc.setOnClickListener{
             val uri = Uri.parse(getString(R.string.url_susi_skill_cms))
             launchCustomtTab(uri)
-        })
+        }
 
-        susi_report_issues_desc.setOnClickListener({
+        susi_report_issues_desc.setOnClickListener{
             val uri = Uri.parse(getString(R.string.url_susi_report_issue))
             launchCustomtTab(uri)
-        })
-        susi_license_info_desc.setOnClickListener({
+        }
+        susi_license_info_desc.setOnClickListener{
             val uri = Uri.parse(getString(R.string.url_susi_license))
             launchCustomtTab(uri)
-        })
+        }
         super.onViewCreated(view, savedInstanceState)
     }
 
