d3c3d71d51cf5f045b3dad6d22eceaaf690b1939
Nilesh <nk966331@gmail.com>
2019-01-13 15:12:05 +0530

    Changes in BASE_URL and URL_SCHEME using mapBox instead of OpenStreetMap

--- app/src/main/java/org/fossasia/susi/ai/helper/MapHelper.kt
+++ app/src/main/java/org/fossasia/susi/ai/helper/MapHelper.kt
@@ -41,15 +41,16 @@ class MapHelper
             val latitude = mapData.latitude
             val longitude = mapData.longitude
             val zoom = mapData.zoom
-            mapURL = String.format(URL_SCHEME, latitude, longitude, zoom, width, height)
+            mapURL = String.format(URL_SCHEME, longitude, latitude, zoom, width, height)
             webLink = String.format(MAP_URL_SCHEME, zoom, latitude, longitude)
             isParseSuccessful = true
         }
     }
 
     companion object {
-        private const val BASE_URL = "http://staticmap.openstreetmap.de/staticmap.php?"
-        private val URL_SCHEME = BASE_URL + "center=%f,%f&zoom=%f&size=%sx%s"
+        val MY_TOKEN="pk.eyJ1IjoibmlsMTIzIiwiYSI6ImNqcXVkMWl1aDA2dmc0Mm10ZjBiYjJ1ZnUifQ.C7pKaawp7pV4uG9rpY9GjQ" //create your own token by register on www.mapbox.com
+        private const val BASE_URL = "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/"
+        private var URL_SCHEME = BASE_URL + "%f,%f,%f/%sx%s?access_token="+ MY_TOKEN
         private const val MAP_URL = "https://www.openstreetmap.org/#map="
         private val MAP_URL_SCHEME = "$MAP_URL%f/%f/%f"
         private const val width = 340
