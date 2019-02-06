e48cb98a50c25c2f3911e4b0ebac1c823be9808e
Naman Jain <naman653@gmail.com>
2019-02-07 02:23:51 +0530

    Added relevant response when trying to log in with inactive or not verified account

--- app/src/main/java/org/fossasia/susi/ai/login/LoginPresenter.kt
+++ app/src/main/java/org/fossasia/susi/ai/login/LoginPresenter.kt
@@ -144,6 +144,10 @@ class LoginPresenter(loginActivity: LoginActivity) : ILoginPresenter, ILoginMode
             loginView?.showProgress(false)
             loginView?.onLoginError(utilModel.getString(R.string.email_not_registered_title),
                     utilModel.getString(R.string.email_not_registered))
+        } else if (response.code() == 403) {
+            loginView?.showProgress(false)
+            loginView?.onLoginError(utilModel.getString(R.string.unactivate_user),
+                    utilModel.getString(R.string.error_unactivated_user_message));
         } else {
             loginView?.showProgress(false)
             loginView?.onLoginError("${response.code()} " + utilModel.getString(R.string.error),
