7c2752c4a7bc1f848ed8e1db37e26a1a305f1ff9
chanmol1999 <chanmol1999@gmail.com>
2019-02-16 21:42:34 +0530

    Updated on backpress just like other activities

--- app/src/main/java/org/fossasia/susi/ai/skills/feedback/FeedbackActivity.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/feedback/FeedbackActivity.kt
@@ -1,12 +1,31 @@
 package org.fossasia.susi.ai.skills.feedback
 
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v4.app.NavUtils
+import android.support.v4.app.NotificationCompat
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_feedback.rvAllFeedback
+import kotlinx.android.synthetic.main.fragment_group_wise_skill_listing.*
+import kotlinx.android.synthetic.main.item_skill_group_list.*
 import org.fossasia.susi.ai.R
+import org.fossasia.susi.ai.chat.ChatActivity
+import org.fossasia.susi.ai.dataclasses.GroupWiseSkills
+import org.fossasia.susi.ai.device.deviceconnect.DeviceConnectFragment
 import org.fossasia.susi.ai.rest.responses.susi.GetSkillFeedbackResponse
+import org.fossasia.susi.ai.skills.SkillsActivity
 import org.fossasia.susi.ai.skills.feedback.adapters.recycleradapters.AllReviewsAdapter
+import org.fossasia.susi.ai.skills.groupwiseskills.GroupWiseSkillsFragment
+import org.fossasia.susi.ai.skills.skilllisting.SkillListingFragment
 
 /**
  *
@@ -16,7 +35,6 @@ class FeedbackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feedback)
-
         val feedbackResponse = intent.extras.get("feedbackResponse") as GetSkillFeedbackResponse
         if (feedbackResponse != null) {
             title = feedbackResponse.skillName.capitalize() + " " + getString(R.string.reviews)
@@ -27,10 +45,21 @@ class FeedbackActivity : AppCompatActivity() {
                 rvAllFeedback.adapter = AllReviewsAdapter(this, feedbackResponse.feedbackList)
             }
         }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onBackPressed() {
-        overridePendingTransition(R.anim.trans_right_in, R.anim.trans_right_out)
+        super.onBackPressed()
         finish()
     }
 }
