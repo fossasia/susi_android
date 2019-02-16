77efa5162d5c722fbb7509ec66df9aa289b66b0a
chanmol1999 <chanmol1999@gmail.com>
2019-02-16 21:48:07 +0530

    Removed unneccesary lines

--- app/src/main/java/org/fossasia/susi/ai/skills/feedback/FeedbackActivity.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/feedback/FeedbackActivity.kt
@@ -1,32 +1,13 @@
 package org.fossasia.susi.ai.skills.feedback
 
-import android.app.PendingIntent
-import android.app.TaskStackBuilder
-import android.content.Context
-import android.content.Intent
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
-import android.support.v4.app.NavUtils
-import android.support.v4.app.NotificationCompat
-import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_feedback.rvAllFeedback
-import kotlinx.android.synthetic.main.fragment_group_wise_skill_listing.*
-import kotlinx.android.synthetic.main.item_skill_group_list.*
 import org.fossasia.susi.ai.R
-import org.fossasia.susi.ai.chat.ChatActivity
-import org.fossasia.susi.ai.dataclasses.GroupWiseSkills
-import org.fossasia.susi.ai.device.deviceconnect.DeviceConnectFragment
 import org.fossasia.susi.ai.rest.responses.susi.GetSkillFeedbackResponse
-import org.fossasia.susi.ai.skills.SkillsActivity
 import org.fossasia.susi.ai.skills.feedback.adapters.recycleradapters.AllReviewsAdapter
-import org.fossasia.susi.ai.skills.groupwiseskills.GroupWiseSkillsFragment
-import org.fossasia.susi.ai.skills.skilllisting.SkillListingFragment
-
 /**
  *
  * Created by arundhati24 on 27/06/2018
@@ -45,7 +26,6 @@ class FeedbackActivity : AppCompatActivity() {
                 rvAllFeedback.adapter = AllReviewsAdapter(this, feedbackResponse.feedbackList)
             }
         }
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -59,6 +39,7 @@ class FeedbackActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
+        overridePendingTransition(R.anim.trans_right_in, R.anim.trans_right_out)
         super.onBackPressed()
         finish()
     }
