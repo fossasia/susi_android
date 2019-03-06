9d8ccb7fd5c640fd5c7791e4a1d371974309ed93
BHAVNA HARITSA <bhavnaharitsa@gmail.com>
2019-03-06 07:35:53 +0530

    removed spaces

--- app/src/main/java/org/fossasia/susi/ai/skills/settings/contract/CustomPreferenceCategory.java
+++ app/src/main/java/org/fossasia/susi/ai/skills/settings/contract/CustomPreferenceCategory.java
@@ -6,40 +6,25 @@ import android.graphics.Color;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceViewHolder;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import org.fossasia.susi.ai.R;
 
 public class CustomPreferenceCategory extends PreferenceCategory {
-
-    @TargetApi(21)
-    public CustomPreferenceCategory(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        this.init(context, attrs);
-    }
-
-    public CustomPreferenceCategory(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        this.init(context, attrs);
-    }
-
+   public CustomPreferenceCategory(Context context){
+       super(context);
+   }
     public CustomPreferenceCategory(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.init(context, attrs);
     }
-
-    public CustomPreferenceCategory(Context context) {
-        super(context);
-        this.init(context, (AttributeSet) null);
+    public CustomPreferenceCategory(Context context, AttributeSet attrs,int defyStlye) {
+        super(context, attrs,defyStlye);
     }
-
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
         TextView title = (TextView) holder.itemView;
-        title.setTextColor(Color.BLUE);
-    }
-
-    private void init(Context context, AttributeSet attrs) {
-        this.setLayoutResource(R.layout.preference_category);
+        title.setTextColor(getContext().getResources().getColor(R.color.md_blue_A400));
     }
 }
