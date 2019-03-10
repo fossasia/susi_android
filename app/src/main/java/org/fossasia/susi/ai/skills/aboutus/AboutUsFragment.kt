439626324d162418abd85133fea0122922b1ab94
Amartya Mondal <hayyoulistentome@gmail.com>
2019-03-10 19:49:53 +0530

    Removed unwanted lines, spaces

--- app/src/main/java/org/fossasia/susi/ai/skills/aboutus/AboutUsFragment.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/aboutus/AboutUsFragment.kt
@@ -29,9 +29,9 @@ class AboutUsFragment : Fragment() {
 
     @NonNull
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val thisActivity = activity
         if (thisActivity is SkillsActivity) thisActivity.title = getString(R.string.action_about_us)
@@ -44,61 +44,61 @@ class AboutUsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         //aboutUs textview got spanned
         val aboutUsString = SpannableString(about_susi.text)
-        val clickableSpan=object : ClickableSpan(){
+        val clickableSpan = object : ClickableSpan() {
             override fun onClick(textView: View) {
                 val uri = Uri.parse(getString(R.string.url_about_susi))
                 launchCustomtTab(uri)
             }
         }
-        aboutUsString.setSpan(clickableSpan, 0, 7 , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        aboutUsString.setSpan(clickableSpan, 0, 7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         about_susi.text = aboutUsString
         about_susi.movementMethod = LinkMovementMethod.getInstance()
 
         //Contributors
         val contributorsString = SpannableString(contributors_desc.text)
-        val contributorsSpan=object : ClickableSpan(){
+        val contributorsSpan = object : ClickableSpan() {
             override fun onClick(textView: View) {
                 val uri = Uri.parse(getString(R.string.url_susi_contributors))
                 launchCustomtTab(uri)
             }
         }
-        contributorsString.setSpan(contributorsSpan, 13, 25 , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        contributorsString.setSpan(contributorsSpan, 13, 25, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         contributors_desc.text = contributorsString
         contributors_desc.movementMethod = LinkMovementMethod.getInstance()
 
         //SUSI_SKILLS_CMS_DESC
         val skillsString = SpannableString(susi_skill_cms_desc.text)
-        val skillsSpan=object : ClickableSpan(){
+        val skillsSpan = object : ClickableSpan() {
             override fun onClick(textView: View) {
                 val uri = Uri.parse(getString(R.string.url_susi_skill_cms))
                 launchCustomtTab(uri)
             }
         }
-        skillsString.setSpan(skillsSpan, 71, 86 , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        skillsString.setSpan(skillsSpan, 71, 86, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         susi_skill_cms_desc.text = skillsString
         susi_skill_cms_desc.movementMethod = LinkMovementMethod.getInstance()
 
         //SUSI license info description
         val licenseString = SpannableString(susi_license_info_desc.text)
-        val licenseSpan=object : ClickableSpan(){
+        val licenseSpan = object : ClickableSpan() {
             override fun onClick(textView: View) {
                 val uri = Uri.parse(getString(R.string.url_susi_license))
                 launchCustomtTab(uri)
             }
         }
-        licenseString.setSpan(licenseSpan, 83, 94 , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        licenseString.setSpan(licenseSpan, 83, 94, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         susi_license_info_desc.text = licenseString
         susi_license_info_desc.movementMethod = LinkMovementMethod.getInstance()
 
         //SUSI report info description
         val reportString = SpannableString(susi_report_issues_desc.text)
-        val reportSpan=object : ClickableSpan(){
+        val reportSpan = object : ClickableSpan() {
             override fun onClick(textView: View) {
                 val uri = Uri.parse(getString(R.string.url_susi_report_issue))
                 launchCustomtTab(uri)
             }
         }
-        reportString.setSpan(reportSpan, 32, 63 , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        reportString.setSpan(reportSpan, 32, 63, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         susi_report_issues_desc.text = reportString
         susi_report_issues_desc.movementMethod = LinkMovementMethod.getInstance()
 
@@ -110,13 +110,6 @@ class AboutUsFragment : Fragment() {
             CustomTabsIntent.Builder().build().launchUrl(context, uri) //launching through custom tabs
         } catch (e: Exception) {
             Toast.makeText(context, getString(R.string.link_unavailable), Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun htmlConverter(resourceId: Int): Spanned {
-        return when {
-            Build.VERSION.SDK_INT>24 -> Html.fromHtml(getString(resourceId), Html.FROM_HTML_OPTION_USE_CSS_COLORS)
-            else -> @Suppress("DEPRECATION") Html.fromHtml(getString(resourceId))
         }
     }
 }
