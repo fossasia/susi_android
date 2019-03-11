55282b500f83aa5527ef6ad9455bfe99115626f5
Amartya Mondal <hayyoulistentome@gmail.com>
2019-03-11 10:43:40 +0530

    Removed wildcard import

--- app/src/main/java/org/fossasia/susi/ai/skills/aboutus/AboutUsFragment.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/aboutus/AboutUsFragment.kt
@@ -13,7 +13,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_about_us.about_susi
 import kotlinx.android.synthetic.main.fragment_about_us.contributors_desc
 import kotlinx.android.synthetic.main.fragment_about_us.susi_skill_cms_desc
