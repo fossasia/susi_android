15841d473dacd4a308cf79ae148e0439f5f7f1e9
chanmol1999 <chanmol1999@gmail.com>
2019-03-24 18:28:51 +0530

    Revert "Removed unnecessary spaces"
    This reverts commit f583d10c9cfb66cb50ded9128bfbba33fdd5d9bf.

--- app/src/main/java/org/fossasia/susi/ai/skills/feedback/FeedbackActivity.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/feedback/FeedbackActivity.kt
@@ -18,9 +18,11 @@ class FeedbackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feedback)
+
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
         val feedbackResponse: GetSkillFeedbackResponse? = intent.extras.get("feedbackResponse") as GetSkillFeedbackResponse
+
         if (feedbackResponse != null) {
             title = feedbackResponse.skillName.capitalize() + " " + getString(R.string.reviews)
             if (feedbackResponse.feedbackList.isNullOrEmpty()) {
