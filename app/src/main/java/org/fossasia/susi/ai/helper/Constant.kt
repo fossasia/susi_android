e05c092914987708da23a155c5344fc0f73cc9fc
Amartya Mondal <hayyoulistentome@gmail.com>
2019-03-26 18:47:36 +0530

    Spotless check made

--- app/src/main/java/org/fossasia/susi/ai/helper/Constant.kt
+++ app/src/main/java/org/fossasia/susi/ai/helper/Constant.kt
@@ -86,5 +86,5 @@ object Constant {
     //know more url
     const val susi_know_more_url = "https://dev.susi.ai/"
 
-    const val FROM_SIGN_UP_ACTIVITY="from_sign_up_activity"
+    const val FROM_SIGN_UP_ACTIVITY = "from_sign_up_activity"
 }
--- app/src/main/java/org/fossasia/susi/ai/signup/SignUpActivity.kt
+++ app/src/main/java/org/fossasia/susi/ai/signup/SignUpActivity.kt
@@ -215,7 +215,7 @@ class SignUpActivity : AppCompatActivity(), ISignUpView {
     private fun signUpToTermsConditionPage() {
         signUpToTermsCondition.setOnClickListener {
             val intent = Intent(this@SignUpActivity, SkillsActivity::class.java)
-            intent.putExtra(Constant.FROM_SIGN_UP_ACTIVITY,true)
+            intent.putExtra(Constant.FROM_SIGN_UP_ACTIVITY, true)
             startActivity(intent)
         }
     }
