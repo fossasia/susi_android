695d18dd2a759a1a7c1e30232e3204f661763eaa
Amartya Mondal <hayyoulistentome@gmail.com>
2019-03-26 19:17:33 +0530

    Add visit website

--- app/src/main/java/org/fossasia/susi/ai/helper/Constant.kt
+++ app/src/main/java/org/fossasia/susi/ai/helper/Constant.kt
@@ -24,6 +24,7 @@ object Constant {
     const val LOGIN_LOGOUT = "login_logout"
     const val RESET_PASSWORD = "reset_password"
     const val CHANGE_PASSWORD = "Change Password"
+    const val VISIT_WEBSITE = "visit_website"
 
     const val SAVED_EMAIL = "saved_email"
     const val SAVE_EMAIL = "save_email"
@@ -85,4 +86,7 @@ object Constant {
 
     //know more url
     const val susi_know_more_url = "https://dev.susi.ai/"
+
+    //Visit Website Url
+    const val SUSI_VISIT_WEBSITE = "https://chat.susi.ai/"
 }
