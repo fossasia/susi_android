c86486c0776bc511bfc863409d6159c7503266e5
naman653 <naman653@gmail.com>
2019-03-31 11:19:28 +0530

    fix: spotless check

--- app/src/main/java/org/fossasia/susi/ai/skills/settings/ChatSettingsFragment.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/settings/ChatSettingsFragment.kt
@@ -1,7 +1,6 @@
 package org.fossasia.susi.ai.skills.settings
 
 import android.Manifest
-import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
