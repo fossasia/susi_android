6a29447b83d1c849969ab76e90f92ea6be41aa63
Amartya Mondal <hayyoulistentome@gmail.com>
2019-04-04 09:19:48 +0530

    Queries made clickable

--- app/src/main/java/org/fossasia/susi/ai/chat/adapters/recycleradapters/VoiceCommandsAdapter.kt
+++ app/src/main/java/org/fossasia/susi/ai/chat/adapters/recycleradapters/VoiceCommandsAdapter.kt
@@ -5,23 +5,44 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.item_voice_commands.view.*
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.item_voice_commands.view.voiceCommand
 import org.fossasia.susi.ai.R
+import org.fossasia.susi.ai.chat.ChatPresenter
+import org.fossasia.susi.ai.chat.contract.IChatPresenter
 
-class VoiceCommandsAdapter(val items: ArrayList<String>, val context: Context?) : RecyclerView.Adapter<ViewHolder>() {
+class VoiceCommandsAdapter(val items: ArrayList<String>, val context: Context?) : RecyclerView.Adapter<VoiceCommandsAdapter.ViewHolder>() {
+    lateinit var chatPresenter: IChatPresenter
+
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
+        chatPresenter = ChatPresenter(context as Context)
         return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_voice_commands, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         holder?.voiceCommand?.text = items.get(position)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
-}
 
-class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val voiceCommand = view.voiceCommand
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val voiceCommand: TextView = view.voiceCommand
+
+        init {
+            view.setOnClickListener {
+                Toast.makeText(context, items[adapterPosition] + "is Clicked", Toast.LENGTH_LONG).show()
+
+                val chatMessage = items[adapterPosition]
+                val splits = chatMessage.split("\n".toRegex()).dropLastWhile { it.isEmpty() }
+                val message = splits.joinToString(" ")
+                if (!chatMessage.isEmpty()) {
+                    chatPresenter.sendMessage(message, items[adapterPosition])
+                }
+            }
+        }
+    }
 }
