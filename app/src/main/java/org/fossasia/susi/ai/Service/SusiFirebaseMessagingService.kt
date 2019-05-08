44fc00b76c26b161df0d72c42fb05a21b8420f98
Amartya Mondal <hayyoulistentome@gmail.com>
2019-05-08 21:40:29 +0530

    CIrcle ci fixed

--- app/src/main/java/org/fossasia/susi/ai/Service/SusiFirebaseMessagingService.kt
+++ app/src/main/java/org/fossasia/susi/ai/Service/SusiFirebaseMessagingService.kt
@@ -8,51 +8,49 @@ import android.graphics.Color
 import android.os.Build
 import android.support.v4.app.NotificationCompat
 import android.util.Log
-import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import org.fossasia.susi.ai.R
 
-class SusiFirebaseMessagingService : FirebaseMessagingService(){
+class SusiFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-        if(remoteMessage.data!=null){
+        if (remoteMessage.data != null) {
             sendNotification(remoteMessage)
         }
     }
 
     override fun onNewToken(token: String?) {
         super.onNewToken(token)
-        Log.e("FCMTOKEN","Token- "+ token)
+        Log.e("FCMTOKEN", "Token- " + token)
     }
     private fun sendNotification(remoteMessage: RemoteMessage) {
-        val title=remoteMessage.data["tittle"]
-        val content =remoteMessage.data["content"]
+        val title = remoteMessage.data["tittle"]
+        val content = remoteMessage.data["content"]
 
-        val notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val NOTIFICATION_CHANNEL_ID="Susi"
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val NOTIFICATION_CHANNEL_ID = "Susi"
 
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O)
-        {
-            val notificationChannel= NotificationChannel(NOTIFICATION_CHANNEL_ID,
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(NOTIFICATION_CHANNEL_ID,
                     "Susi Notification",
                     NotificationManager.IMPORTANCE_HIGH)
 
-            notificationChannel.description="Susi Notification Channel"
+            notificationChannel.description = "Susi Notification Channel"
             notificationChannel.enableLights(true)
-            notificationChannel.lightColor=(Color.RED)
+            notificationChannel.lightColor = (Color.RED)
             notificationChannel.enableVibration(true)
-            notificationChannel.vibrationPattern= longArrayOf(0,1000,500,500)
+            notificationChannel.vibrationPattern = longArrayOf(0, 1000, 500, 500)
 
             notificationManager.createNotificationChannel(notificationChannel)
         }
-        val notificationBuilder= NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID)
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
         notificationBuilder.setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setWhen(System.currentTimeMillis())
                 .setSmallIcon(R.drawable.ic_susi_blue)
                 .setContentTitle(title)
                 .setContentText(content)
-        notificationManager.notify(1,notificationBuilder.build())
+        notificationManager.notify(1, notificationBuilder.build())
     }
 }
