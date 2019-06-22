c87f1b6e67b83b9a4f7ee350a1279d2f59d4c8df
Mohit Kumar <mohitkumar2k15@dtu.ac.in>
2019-06-22 17:49:39 +0530

    chore: refactor rateskill code in viewholders (#2270)
    * refactor: rating call made only once
    
    * fix the spotless check
    
    * fix: failing ci build

--- app/src/main/java/org/fossasia/susi/ai/chat/adapters/viewholders/MessageViewHolder.kt
+++ app/src/main/java/org/fossasia/susi/ai/chat/adapters/viewholders/MessageViewHolder.kt
@@ -1,15 +1,34 @@
 package org.fossasia.susi.ai.chat.adapters.viewholders
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import io.realm.Realm
+import kotterknife.bindView
+import org.fossasia.susi.ai.R
+import org.fossasia.susi.ai.chat.ParseSusiResponseHelper
+import org.fossasia.susi.ai.data.model.ChatMessage
+import org.fossasia.susi.ai.helper.Constant
+import org.fossasia.susi.ai.rest.ClientBuilder
+import org.fossasia.susi.ai.rest.responses.susi.SkillRatingResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import timber.log.Timber
 
 abstract class MessageViewHolder(itemView: View, private val listener: ClickListener?) :
-    RecyclerView.ViewHolder(itemView), View.OnClickListener, View.OnLongClickListener {
+        RecyclerView.ViewHolder(itemView), View.OnClickListener, View.OnLongClickListener {
 
     init {
         itemView.setOnClickListener(this)
         itemView.setOnLongClickListener(this)
     }
+
+    val thumbsUp: ImageView by bindView(R.id.thumbs_up)
+    val thumbsDown: ImageView by bindView(R.id.thumbs_down)
+    var model: ChatMessage? = null
 
     override fun onClick(view: View) {
         listener?.onItemClicked(adapterPosition)
@@ -37,5 +56,54 @@ abstract class MessageViewHolder(itemView: View, private val listener: ClickList
          * @return the boolean
          */
         fun onItemLongClicked(position: Int): Boolean
+    }
+
+    fun setRating(rating: Boolean, thumbsUp: Boolean) {
+        val realm = Realm.getDefaultInstance()
+        realm.beginTransaction()
+        if (thumbsUp) {
+            model?.isPositiveRated = rating
+        } else {
+            model?.isNegativeRated = rating
+        }
+        realm.commitTransaction()
+    }
+
+    fun updateRating(polarity: String) {
+        when (polarity) {
+            Constant.POSITIVE -> {
+                thumbsUp.setImageResource(R.drawable.thumbs_up_outline)
+                setRating(false, true)
+            }
+            Constant.NEGATIVE -> {
+                thumbsDown.setImageResource(R.drawable.thumbs_down_outline)
+                setRating(false, false)
+            }
+        }
+    }
+
+    fun rateSusiSkill(polarity: String, locationUrl: String, context: Context) {
+        val queryObject = ParseSusiResponseHelper.getSkillRatingQuery(locationUrl)?.copy(rating = polarity)
+                ?: return
+
+        val ratingResponseCall = ClientBuilder.rateSkillCall(queryObject)
+
+        ratingResponseCall.enqueue(object : Callback<SkillRatingResponse> {
+            override fun onResponse(responseCall: Call<SkillRatingResponse>, response: Response<SkillRatingResponse>) {
+                if (!response.isSuccessful || response.body() == null) {
+                    updateRating(polarity)
+                    Toast.makeText(context, context.getString(R.string.error_rating), Toast.LENGTH_SHORT).show()
+                } else {
+                    Timber.d("Rating successful")
+                    Toast.makeText(context, R.string.rate_chat, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(responseCall: Call<SkillRatingResponse>, t: Throwable) {
+                Timber.e(t)
+                updateRating(polarity)
+                Toast.makeText(context, context.getString(R.string.error_rating), Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
