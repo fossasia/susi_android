04d039879552c658bd9404489affa28f29c95a59
Amartya Mondal <hayyoulistentome@gmail.com>
2019-07-26 00:33:33 +0530

    feat: Add editing of room ui

--- app/src/main/java/org/fossasia/susi/ai/device/viewdevice/ViewDeviceFragment.kt
+++ app/src/main/java/org/fossasia/susi/ai/device/viewdevice/ViewDeviceFragment.kt
@@ -5,7 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_view_device.view_device
+import kotlinx.android.synthetic.main.edit_name_layout.*
+import kotlinx.android.synthetic.main.fragment_view_device.*
 import kotlinx.android.synthetic.main.view_device_layout.spk_location
 import kotlinx.android.synthetic.main.view_device_layout.spk_email
 import kotlinx.android.synthetic.main.view_device_layout.spk_macid
@@ -20,7 +21,11 @@ import org.fossasia.susi.ai.rest.responses.susi.Device
 class ViewDeviceFragment : Fragment(), IViewDeviceView {
 
     private var macId: String? = null
-    private var device: Device? = null
+    private lateinit var name: String
+    private lateinit var room: String
+    private lateinit var latitude: String
+    private lateinit var longitude: String
+
     private lateinit var viewDevicePresenter: ViewDevicePresenter
 
     override fun onCreateView(
@@ -36,20 +41,49 @@ class ViewDeviceFragment : Fragment(), IViewDeviceView {
 
         val extras = activity?.intent?.extras
         macId = extras?.getString("macId")
-        device = extras?.get("deviceDetails") as Device?
+        val device = extras?.get("deviceDetails") as Device?
+        name = device?.name.toString()
+        room = device?.room.toString()
+        latitude = device?.geolocation?.latitude.toString()
+        longitude = device?.geolocation?.longitude.toString()
+
         viewDevicePresenter = ViewDevicePresenter()
         viewDevicePresenter.onAttach(this, macId, device)
 
+        initialSetUp()
+    }
+
+    override fun initialSetUp() {
         viewDetails()
+
+        spk_name.setOnClickListener {
+            changeSpeakerName()
+        }
     }
 
     // Function to show the device information
     override fun viewDetails() {
         view_device.visibility = View.VISIBLE
+        change_name.visibility = View.GONE
         spk_email.text = PrefManager.getStringSet(Constant.SAVED_EMAIL)?.iterator()?.next().toString()
         spk_macid.text = macId
-        spk_room.text = device?.room
-        spk_name.text = device?.name
-        spk_location.text = device?.geolocation?.latitude + ", " + device?.geolocation?.longitude
+        spk_room.text = room
+        spk_name.text = name
+        spk_location.text = latitude + ", " + longitude
+    }
+
+    override fun changeSpeakerName() {
+        change_name.visibility = View.VISIBLE
+        view_device.visibility = View.GONE
+
+        name_cancel.setOnClickListener {
+            viewDetails()
+        }
+
+        name_save.setOnClickListener {
+            // For testing purpose
+            name = name_new.text.toString()
+            viewDetails()
+        }
     }
 }
--- app/src/main/java/org/fossasia/susi/ai/device/viewdevice/contract/IViewDeviceView.kt
+++ app/src/main/java/org/fossasia/susi/ai/device/viewdevice/contract/IViewDeviceView.kt
@@ -3,4 +3,8 @@ package org.fossasia.susi.ai.device.viewdevice.contract
 interface IViewDeviceView {
 
     fun viewDetails()
+
+    fun initialSetUp()
+
+    fun changeSpeakerName()
 }
