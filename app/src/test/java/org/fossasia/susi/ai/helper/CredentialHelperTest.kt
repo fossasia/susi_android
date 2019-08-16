d01dd621fa4f28dbbb5f0e10975d5a6e95a24cea
Amartya Mondal <hayyoulistentome@gmail.com>
2019-08-16 21:56:16 +0530

    fix: Make suggested changes

--- app/src/test/java/org/fossasia/susi/ai/helper/CredentialHelperTest.kt
+++ app/src/test/java/org/fossasia/susi/ai/helper/CredentialHelperTest.kt
@@ -2,16 +2,14 @@ package org.fossasia.susi.ai.helper
 
 import org.junit.Assert
 import org.junit.Test
-import timber.log.Timber
 
 class CredentialHelperTest {
     @Test
     fun testSignupPassword() {
-        Timber.d("running password test..")
-        Assert.assertEquals(false, CredentialHelper.isPasswordValid("123345602"))
-        Assert.assertEquals(false, CredentialHelper.isPasswordValid("123345602@"))
-        Assert.assertEquals(false, CredentialHelper.isPasswordValid("123345602a"))
-        Assert.assertEquals(false, CredentialHelper.isPasswordValid("123345602@a"))
-        Assert.assertEquals(true, CredentialHelper.isPasswordValid("123345602@As"))
+        Assert.assertFalse(CredentialHelper.isPasswordValid("123345602"))
+        Assert.assertFalse(CredentialHelper.isPasswordValid("123345602@"))
+        Assert.assertFalse(CredentialHelper.isPasswordValid("123345602a"))
+        Assert.assertFalse(CredentialHelper.isPasswordValid("123345602@a"))
+        Assert.assertTrue(CredentialHelper.isPasswordValid("123345602@As"))
     }
 }
