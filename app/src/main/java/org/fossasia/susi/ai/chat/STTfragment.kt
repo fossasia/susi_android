be27418a119937f9fddf8287dbaa0021f1a168a5
Shubham Kumar <shubham.kumar1739@gmail.com>
2020-02-07 18:02:23 +0530

    fix: Speech to text button on chat activity not working #2469

--- app/src/main/java/org/fossasia/susi/ai/chat/STTfragment.kt
+++ app/src/main/java/org/fossasia/susi/ai/chat/STTfragment.kt
@@ -170,7 +170,6 @@ class STTFragment : Fragment() {
         recognizer.startListening(intent)
     }
     private fun restoreActivityState() {
-
         if ((activity as ChatActivity).recordingThread != null) {
             chatPresenter.startHotwordDetection()
         }
