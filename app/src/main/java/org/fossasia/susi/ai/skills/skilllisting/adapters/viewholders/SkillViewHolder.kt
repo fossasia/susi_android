9f765a9cbfa24644162dde01a62ec269a2b53b4c
Shubham Kumar <shubham.kumar1739@gmail.com>
2020-02-14 02:21:22 +0530

    fix: Add TRY button in the Skills List #2493
    Add a method tryButtonClicked() in the SkillFragmentCallback
    which plays the skill when the Try button is clicked from the Skills List
    itself.
    
    Implement the method in the skills activity to send the skill data to SUSI
    in ChatActivity.

--- app/src/main/java/org/fossasia/susi/ai/skills/skilllisting/adapters/viewholders/SkillViewHolder.kt
+++ app/src/main/java/org/fossasia/susi/ai/skills/skilllisting/adapters/viewholders/SkillViewHolder.kt
@@ -2,9 +2,11 @@ package org.fossasia.susi.ai.skills.skilllisting.adapters.viewholders
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_skill_details.*
 import kotterknife.bindView
 import org.fossasia.susi.ai.R
 
@@ -15,6 +17,7 @@ class SkillViewHolder(itemView: View, private val listener: ClickListener?) : Re
     val skillPreviewExample: TextView by bindView(R.id.skill_preview_example)
     val skillRatingBar: RatingBar by bindView(R.id.cv_rating_bar)
     val totalRatings: TextView by bindView(R.id.cv_total_ratings)
+    val skillItemTryButton: Button by bindView(R.id.skillItemTryButton)
 
     init {
         itemView.setOnClickListener(this)
